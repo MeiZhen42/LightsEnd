19a1d2c0fd11f09395e452b4393588d8d8394178
SuperTheUnderDog <136886239+SuperTheUnderDog@users.noreply.github.com>
2024-07-31 22:54:26 +0300

    Update global_strings.gd
    Finished adding current texture paths

--- CurrentVersion/scripts/global_strings.gd
+++ CurrentVersion/scripts/global_strings.gd
@@ -1,6 +1,72 @@
 class_name GlobalStrings
 
 #texture paths
+const texture_path = {
+	potion = {
+		blue_circle = texture_potion_blue_circle,
+		green_circle = texture_potion_green_circle,
+		yellow_circle = texture_potion_yellow_circle,
+		blue_triangle = texture_potion_blue_triangle,
+		green_triangle = texture_potion_green_triangle,
+		yellow_triangle = texture_potion_yellow_triangle,
+		blue_square = texture_potion_blue_square,
+		green_square = texture_potion_green_square,
+		yellow_square = texture_potion_yellow_square,
+		failed = texture_potion_failed
+	},
+
+	overwold_resource = {
+		ingredient = {
+			nox_lily = texture_ingredient_noxLily,
+			hollow_tree = texture_ingredient_hollowTree,
+			weeping_dandelion = texture_ingredient_weepingDandelion,
+			dew_tree = texture_ingredient_dewTree,
+			murkberry_bush = texture_ingredient_murkberryBush,
+			gloomstone = texture_ingredient_gloomstones
+		},
+		environmental = {
+			trees = {
+				live = texture_environment_object_outside_tree,
+				dark = texture_environment_object_outside_darkTree
+			}
+		},
+		tileset = {
+			ground = texture_tileset_ground
+		}
+	},
+	
+	npc = {
+		bar_owner = {
+			overworld_sprite = "res://characters/Bar_Owner_Overworld.png"
+		},
+		tavern_customers = {
+			sprite_sheet = "res://characters/SpriteSheet.png"
+		}
+	},
+	
+	player = {
+		sprite_sheet = "res://characters/mainPlayer.png"
+	},
+	
+	enemy = {
+		minion_goober = {
+			sprite = "res://characters/MinionGoober.png"
+		}
+	},
+	
+	overlay = {
+		npc = {
+			cursed = texture_overlay_npc_shadow
+		}
+	},
+	
+	structure = {
+		tavern = {
+			outside = texture_structure_tavern_outside
+		}
+	}
+}
+
 const texture_potion_blue_circle: String = "res://objects/consumables/BlueCirclePotionAccessible.png" #"res://objects/consumables/BlueCirclePotion.png"
 const texture_potion_green_circle: String = "res://objects/consumables/GreenCirclePotionAccessible.png" #"res://objects/consumables/GreenCirclePotion.png"
 const texture_potion_yellow_circle: String = "res://objects/consumables/YellowCirclePotionAccessible.png" #"res://objects/consumables/YellowCirclePotion.png"
@@ -18,3 +84,13 @@ const texture_ingredient_weepingDandelion: String = "res://objects/WeepingDandel
 const texture_ingredient_dewTree: String = "res://objects/DewTree.png"
 const texture_ingredient_murkberryBush: String = "res://objects/MurkberryBush.png"
 const texture_ingredient_gloomstones: String = "res://objects/Gemstones.png"
+
+const texture_overlay_npc_shadow: String = "res://objects/Shadow.png"
+
+const texture_structure_tavern_outside: String = "res://objects/Tavern2.png"
+
+const texture_environment_object_outside_tree: String = "res://objects/Tree.png"
+const texture_environment_object_outside_darkTree: String = "res://objects/DarkTree.png"
+
+const texture_tileset_ground: String = "res://objects/Ground_Tileset.png"
+
